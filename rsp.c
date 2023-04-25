88b2e107387c2831adb7c1ad337fdd0de78541b8
_kvvak <sirin05137@naver.com>
2023-04-25 08:04:29 +0000

    Change the munmap

--- rsp.c
+++ rsp.c
@@ -19,6 +19,7 @@ pthread_create_t real_pthread_create = 0;
 //static pthread_once_t HOOKING_INITIALIZATION = PTHREAD_ONCE_INIT;
 
 __thread void* extern_stack_ptr = NULL;
+__thread void* smallest_addr_used = NULL;
 
 void *__get_extern_stack_ptr();
 void* thread_function_hooking(void*);
@@ -46,34 +47,26 @@ int pthread_create(pthread_t *restrict thread,
 
 void* thread_function_hooking(void* args){
 	void* extern_sp = __get_extern_stack_ptr();
+	//void* extern_sp = __allocate_extern_stack(DEFAULT_STACK_SIZE);
 	Argument_t argument = *(Argument_t*) args;
 	void* (*origin_function)(void*) = argument.function;
 	void* origin_args = argument.args;
-	asm("mov %0, %%r15;"::"r" (extern_sp):"%r15");
+	//asm("mov %0, %%r15;"::"r" (extern_sp):"%r15");
 	
 	void *retval = origin_function(origin_args);
 
-	/*void* temp_addr;
-	asm("mov %%r15, %0;":"=r" (temp_addr)::);
-	uint64_t used_stack_size = (uint64_t)((char*)extern_sp - (char*)temp_addr);
-	//printf("extern stack ptr : %p\n", extern_sp);
-	//printf("temp addr : %p\n", temp_addr);
-	//printf("used stack size : %ld\n", used_stack_size);
+	uint64_t used_stack_size = (uint64_t)((char*)extern_sp - (char*)smallest_addr_used);
 	int num_page = used_stack_size/4096;
 	if(num_page < 8)
-		num_page =8;
+		num_page = 8;
 
-	if(used_stack_size%4096)
-		num_page = 1+ num_page;
+	else if(used_stack_size%4096)
+		num_page = 1 + num_page;
 
 	if(munmap((void*)((char*)extern_sp-num_page*4096), num_page*4096)==-1){
 		//printf("%d\n", num_page);
 		printf("Unable to release the extern stack\n");
-	}*/
-	if(munmap(extern_sp-DEFAULT_STACK_SIZE, DEFAULT_STACK_SIZE)==-1){
-		printf("Unable to release the extern stack\n");
 	}
-	//mi_free(extern_sp-DEFAULT_STACK_SIZE);
 	return retval;
 }
 
@@ -81,14 +74,30 @@ void *__allocate_extern_stack(size_t size){
 	extern_stack_ptr = mmap(NULL, size, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS | MAP_GROWSDOWN, -1, 0);
 	//extern_stack_ptr = mi_malloc(size);
 	extern_stack_ptr = (void*)((char*)extern_stack_ptr + size);
+	smallest_addr_used = extern_stack_ptr;
 	
 	return extern_stack_ptr;
 }
 
 void *__get_extern_stack_ptr(){
 	if(!extern_stack_ptr){
+		//assert
 		__allocate_extern_stack(DEFAULT_STACK_SIZE);
 	}
 	printf("extern stack pointer : %p\n\n", extern_stack_ptr);
 	return extern_stack_ptr;
 }
+
+void smallest_address_used(){
+	if ((uint64_t)extern_stack_ptr < (uint64_t)smallest_addr_used){
+		smallest_addr_used = extern_stack_ptr;
+	}
+}
+
+void* register_2_memory(void* static_top){
+	extern_stack_ptr = static_top;
+	smallest_address_used();
+	return extern_stack_ptr;
+	//asm("mov %%r15, %0;":"=r"(extern_stack_ptr)::);
+}
+
