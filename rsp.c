ebba9f446cb68d111243bd95e7ca3a2925870f01
_kvvak <sirin05137@naver.com>
2023-05-10 05:59:28 +0000

    Use FS register to extern stack pointer

--- rsp.c
+++ rsp.c
@@ -13,6 +13,7 @@
 #include <sys/syscall.h>
 #include <unistd.h>
 #include <asm/prctl.h>
+#include <string.h>
 
 #define PTHREAD_HOOKING_ERROR \
   fprintf(stderr, "Unable to create pthread library hooks\n"); \
@@ -24,11 +25,19 @@ typedef int (*pthread_create_t)(pthread_t* restrict, const pthread_attr_t* restr
 pthread_create_t real_pthread_create = 0;
 //static pthread_once_t HOOKING_INITIALIZATION = PTHREAD_ONCE_INIT;
 
+struct ExternStackPointer
+{
+	void *pure_ptr;
+	void *housed_ptr;
+};
+
+
 __thread void* extern_stack_ptr = NULL;
 __thread void* smallest_addr_used = NULL;
 
 void *__get_extern_stack_ptr();
 void* thread_function_hooking(void*);
+void MEM2GS(void* test);
 
 typedef struct Argument{
 	void* (*function)(void*);
@@ -55,7 +64,10 @@ void* thread_function_hooking(void* args){
 	//void* extern_sp = __allocate_extern_stack(DEFAULT_STACK_SIZE);
 	Argument_t *argument = (Argument_t*) args;
 	//asm("mov %0, %%r15;"::"r" (extern_sp):"%r15");
-    arch_prctl(ARCH_SET_GS, (uintptr_t)extern_sp);
+    
+	MEM2GS(extern_sp);	
+	
+	//arch_prctl(ARCH_SET_GS, (uintptr_t)extern_sp);
 	
 	void *retval = argument->function(argument->args);
 
@@ -106,7 +118,7 @@ void* register_2_memory(void* static_top){
 	//asm("mov %%r15, %0;":"=r"(extern_stack_ptr)::);
 }
 
-void MEM2GS(void* test){
+/*void MEM2GS(void* test){
 
 	
 	//unsigned val = getauxval(AT_HWCAP2);
@@ -118,12 +130,51 @@ void MEM2GS(void* test){
 	uintptr_t ptrToInt = (uintptr_t)test;
 	//asm("wrgsbase %0"::"r"(ptrToInt));
 	arch_prctl(ARCH_SET_GS, ptrToInt);
+
+	uint64_t temp;
+	asm("movq %%gs:%c[offset], %0" : "=r" (temp): [offset] "i"(0));	
+	printf("1 : 0x%lx\n", temp);
+	uintptr_t a;//=_readgsbase_u64();
+	arch_prctl(ARCH_GET_GS, &a);
+	printf("2 : 0x%lx\n", a);
+	
 	//_writegsbase_u64(ptrToInt);
 	//}
 	//asm("movq %0,%%gs:%c[offset]" : : "r" (test), [offset] "i" (0));
+}*/
+/*
+void MEM2GS(void* test){
+	if ((uint64_t)test < (uint64_t)smallest_addr_used){
+		smallest_addr_used = test;
+	}
+	//void *temp;
+	uint64_t tttt = (uint64_t)test;
+	uintptr_t a;
+	arch_prctl(ARCH_GET_FS, &a);
+	printf("aaa = 0x%lx\n", a);
+	void* temp=(void*)(a+56);// = mmap((void*)(a+160), 8, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+	//printf("temp = %p\n", temp);
+	*(uint64_t*)temp = tttt;
+	//memcpy(temp, &tttt, 8);
+	printf("temp = %p\n", temp);
+	// *(uint64_t*)(a+16) =(uint64_t)test; 
+	//munmap((void*)(a+160), 8);
+	//asm("movq %0, %%gs:%c[offset]" ::"r" ((uint64_t)test), [offset] "i"(16));	
+}
+*/
+void MEM2GS(void* test){
+	if ((uint64_t)test < (uint64_t)smallest_addr_used){
+		smallest_addr_used = test;
+	}	
+	
+	asm("movq %0, %%fs:%c[offset]" ::"r" ((uint64_t)test), [offset] "i"(56));	
+
 }
 
-void* GS2MEM(){
+
+
+
+/*void* GS2MEM(){
 	//unsigned val = getauxval(AT_HWCAP2);
 	void *temp;
 	uintptr_t a;//=_readgsbase_u64();
@@ -134,4 +185,35 @@ void* GS2MEM(){
 	temp = (void*)a;
 	//asm("movq %%gs:%c[offset], %0" : "=r" (temp) :[offset] "i" (0));
 	return temp;
+}*/
+
+/*
+void* GS2MEM(){
+	uint64_t ttttt;
+	uint64_t a;
+	arch_prctl(ARCH_GET_FS, &a);
+	printf("a = 0x%lx\n",a);
+	//a+=16;
+	void* temp =(void*)(a+56);// = mmap((void*)(a+160), 8, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+	//uint64_t* b = (uint64_t*)a;
+	//printf("temptwmp = %p\n", temp);
+	//memcpy(&ttttt, temp, 8);
+	uint64_t* temp2 = (uint64_t*) temp;
+	ttttt = *temp2; 
+
+
+	//uint64_t b = *(uint64_t*)(a+16);
+	printf("ttttt = %p\n", (void*)ttttt);
+	//munmap((void*)(a+160), 8);
+	return (void*)ttttt;
+	//uint64_t temp;
+	//asm("movq %%gs:%c[offset], %0" : "=r" (temp): [offset] "i"(16));
+	//printf("0x%lx\n", temp);
+	//return (void*)temp;
+}*/
+
+void* GS2MEM(){
+	uint64_t temp;
+	asm("movq %%fs:%c[offset], %0" : "=r" (temp) :[offset] "i" (56));
+	return (void*)temp;
 }
